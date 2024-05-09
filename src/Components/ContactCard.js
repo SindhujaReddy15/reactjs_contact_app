8d89e5c44fa220e0351eeb3d0775b807033d8fab
sindhujareddymaryadha <msindhujareddy.15@gmail.com>
2024-05-09 15:59:35 -0500

    modified:   src/Components/ContactCard.js 	modified:   src/Components/ContactList.js

--- src/Components/ContactCard.js
+++ src/Components/ContactCard.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-const CardContact = (props) => {
+
+const ContactCard = (props) => {
+    const {id,name,email} = props.contact;
     return (
         <div className="item">
             <div className="content">
-                <div className="header">{contact.name}</div>
-                <div>{contact.email}</div>
+                <div className="header">{name}</div>
+                <div>{email}</div>
             </div>
             <i className="trash alternate outline icon"
             style={{color:"red",marginTop:"7px" }}
